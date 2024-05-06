33c05309f28792a161d55e2201252b85576740e2
Ea123 <96218748+DuyEa@users.noreply.github.com>
2024-05-06 19:59:36 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
+import AssignDes from "./components/AssignDes";
 import CourseDetail from "./pages/CourseDetail";
+import CourseAssignment from "./pages/CourseAssignment";
+import UserProfile from "./pages/UserProfile";
 import {TeacherProfile} from "./pages/TeacherProfile.jsx";
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="course/:id" element={<CourseDetail/>}/>
                 <Route path="teacher/:id" element={<TeacherProfile/>}/>
+                <Route path="/CourseAssignment" element={<CourseAssignment />} />
+                <Route path="/AssignDes" element={<AssignDes />} />
+                <Route path="/UserProfile" element={<UserProfile />} />
             </Routes>
         </BrowserRouter>
     );
