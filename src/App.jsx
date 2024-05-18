c1f8d0b4b9530e5220acf38e45a42d16a367e94e
Ea123 <96218748+DuyEa@users.noreply.github.com>
2024-05-18 10:30:39 +0700

    Update SignIn/Up.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import CourseAssignment from "./pages/CourseAssignment";
 // import UserProfile from "./pages/UserProfile";
 import {TeacherProfile} from "./pages/TeacherProfile.jsx";
 import VideoCoursePage from "./pages/VideoCoursePage.jsx";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
     return (
@@ -20,6 +22,8 @@ function App() {
                 <Route path="/AssignDes" element={<AssignDes />} />
                 {/*<Route path="/UserProfile" element={<UserProfile />} />*/}
                 <Route path ="course/:id/:videoId" element={<VideoCoursePage/>}/>
+                <Route path="/SignIn" element={<SignIn />} />
+                <Route path="/SignUp" element={<SignUp />} />
 
             </Routes>
         </BrowserRouter>
